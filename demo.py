2949edb454ed1d67dff82458cba453654347d7ea
cerelise <291244924@qq.com>
2023-10-01 23:51:44 +0800

    add python conn mysql

--- demo.py
+++ demo.py
@@ -1,1 +1,9 @@
-print('hello world')
+# print('hello world')
+
+print("Albert “A person”")
+
+num = 18
+print(f"My favourite number is {num}")
+
+message = "Happy " + str(num) + "rd Birthday"
+print(message)
